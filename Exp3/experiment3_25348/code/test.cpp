476442ae466c48be85c115c102eafdc8aabec8a6
shubham-sahoo <shubhamsomnath@gmail.com>
2021-02-15 17:52:45 +0530

    check

--- Exp3/experiment3_25348/code/test.cpp
+++ Exp3/experiment3_25348/code/test.cpp
@@ -89,25 +89,49 @@ uint8_t* convolve(Mat image, double* h, int size, int type)
 					sum += h[posh] * (int)pixel[posim];
 				}
 			}
-			if(sum>max_v)
+			// if(sum>max_v)
+			// {
+			// 	max_v=sum;
+			// }
+			// if(sum<min_v)
+			// {
+			// 	min_v=sum;
+			// }
+			// newimage[i * m + j] = (uint8_t)floor(sum);
+			// if(sum<0)
+			// {
+			// 	sum=0;
+			// }
+			// if(sum>255)
+			// {
+			// 	sum=255;
+			// }
+			// if((type!=7))//&&(type!=8))
+			// 	newimage[i * m + j] = (uint8_t)floor(sum);
+
+			if(type != 7)
 			{
-				max_v=sum;
+				if(sum<0)
+				{
+					sum=0;
+				}
+				if(sum>255)
+				{
+					sum=255;
+				}
+				newimage[i*m + j] = (uint8_t)floor(sum);
+				if(type == 8)
+					newimage[i*m + j] = (uint8_t)(255 - (int)newimage[i*m + j]);
 			}
-			if(sum<min_v)
+			else
 			{
-				min_v=sum;
+				if(sum<0)
+					sum = -sum;
+				if(sum>255)
+					sum = 255;
+				newimage[i*m + j] = (uint8_t)floor(sum);
+				// cout << (int)newimage[i*m + j] << " ";
 			}
-			newimage[i * m + j] = (uint8_t)floor(sum);
-			if(sum<0)
-			{
-				sum=0;
-			}
-			if(sum>255)
-			{
-				sum=255;
-			}
-			if(type!=7)
-				newimage[i * m + j] = (uint8_t)floor(sum);
 		}
 	}
 
@@ -208,24 +232,30 @@ uint8_t* applylaplacian(Mat image, int size, int type)
 {
 	double* h = new double[size * size];
 	int mid = floor((size)/2);
-	double sum = 0;
+	// double sum = 0;
+	// for(int i=0;i<size;i++)
+	// {
+	// 	for(int j=0;j<size;j++)
+	// 	{	
+			
+	// 		if(i==mid && j==mid)
+	// 		{
+	// 			continue;
+	// 		}
+			
+	// 		h[i*size + j] = -floor(floor(size/2)/sqrt(pow(i-mid,2)+pow(j-mid,2)));
+	// 		sum += h[i*size + j];
+			
+	// 	}
+		
+	// }
+	// h[mid*size + mid] = -sum;	
 	for(int i=0;i<size;i++)
 	{
 		for(int j=0;j<size;j++)
-		{	
-			
-			if(i==mid && j==mid)
-			{
-				continue;
-			}
-			
-			h[i*size + j] = -floor(floor(size/2)/sqrt(pow(i-mid,2)+pow(j-mid,2)));
-			sum += h[i*size + j];
-			
-		}
-		
-	}
-	h[mid*size + mid] = -sum;	
+			h[i*size + j] = 1;
+	}	
+	h[mid * size + mid] = -(pow(size,2) - 1);
 
 	return convolve(image, h, size, type);
 }
@@ -234,53 +264,102 @@ uint8_t* applylaplacian(Mat image, int size, int type)
 
 uint8_t* applyLoG(Mat image, int size, int type)
 {
-	uint8_t* pixel = applygaussian(image, size, 1);
-	int n = image.rows;
-	int m = image.cols;
-	Mat res(n, m, CV_8UC1, Scalar(0));
-	res.data = pixel;
-	return applylaplacian(res, size, 7);
+	// uint8_t* pixel = applygaussian(image, size, 1);
+	// int n = image.rows;
+	// int m = image.cols;
+	// Mat res(n, m, CV_8UC1, Scalar(0));
+	// res.data = pixel;
+	// return applylaplacian(res, size, 7);
 
-	// double* h = new double[size * size];
-	// float sigma = 1.0;
-	// // for(int i=0;i<size;i++)
-	// // {
-	// // 	for(int j=0;j<size;j++)
-	// // 	{
-	// // 		int x = abs(i-mid);
-	// // 		int y = abs(j-mid);
-	// // 		float q = 2 * PI * pow(sigma, 2);
-	// // 		int p = pow(x, 2) + pow(y, 2);
-	// // 		h[i*size + j] = (float)(-(1/(PI * pow(sigma, 4))) * (1 - p/q) * exp(-p/q));
-	// // 		h[i*size + j] *= 426.3;
-	// // 		cout << -(1/(PI * pow(sigma, 4))) * (1 - p/q) * exp(-p/q) << " " << i << " " << j << " " << p << endl;
-	// // 	}
-	// // }
-	// int kernelSize = size;
-	// for(int i = -(kernelSize/2); i<=(kernelSize/2); i++)
- //    {
+	double* h = new double[size * size];
 
- //        for(int j = -(kernelSize/2); j<=(kernelSize/2); j++)
- //        {
+	if(size == 3)
+	{
+		h[0] = -1; h[1] = -1; h[2] = -1;
+		h[3] = -1; h[4] = 8; h[5] = -1;
+		h[6] = -1; h[7] = -1; h[8] = -1;
+	}
+	else if(size == 5)
+	{
+		h[0] = -1; h[1] = 3; h[2] = -4; h[3] = -3; h[4] =  -1;
+		h[5] = -3; h[6] = 0; h[7] = 6; h[8] = 0; h[9] =  -3;
+		h[10] = -4; h[11] = 6; h[12] = 20; h[13] = 6; h[14] =  -4;
+		h[15] = -3; h[16] = 0; h[17] = 6; h[18] = 0; h[19] =  -3;
+		h[20] = -1; h[21] = 3; h[22] = -4; h[23] = -3; h[24] =  -1;
+	}
+	else if(size == 7)
+	{
+		h[0] = -2; h[1] = -3; h[2] = -4; h[3] = -6; h[4] = -4; h[5] = -3; h[6] = -2;
+		h[7] = -3; h[8] = -5; h[9] = -4; h[10] = -3; h[11] = -4; h[12] = -5; h[13] = -3;
+		h[14] = -4; h[15] = -4; h[16] = 9; h[17] = 20; h[18] = 9; h[19] = -4; h[20] = -4;
+		h[21] = -6; h[22] = -3; h[23] = 20; h[24] = 36; h[25] = 20; h[26] = -3; h[27] = -6;
+		h[28] = -4; h[29] = -4; h[30] = 9; h[31] = 20; h[32] = 9; h[33] = -4; h[34] = -4;
+		h[35] = -3; h[36] = -5; h[37] = -4; h[38] = -3; h[39] = -4; h[40] = -5; h[41] = -3;
+		h[42] = -2; h[43] = -3; h[44] = -4; h[45] = -6; h[46] = -4; h[47] = -3; h[48] = -2;
 
- //            double L_xy = -1/(PI * pow(sigma,4))*(1 - ((pow(i,2) + pow(j,2))/(2*pow(sigma,2))))*exp(-((pow(i,2) + pow(j,2))/(2*pow(sigma,2))));
- //            L_xy*=426.3;
- //            h[(i + kernelSize/2)*size  + (j + kernelSize/2)] = L_xy;
- //        }
+	}
+	else
+	{
+		h[0] = 0; h[1] = 1; h[2] = 1; h[3] = 2; h[4] = 2; h[5] = 2; h[6] = 1; h[7] = 1; h[8] = 0;
+		h[9] = 1; h[10] = 2; h[11] = 4; h[12] = 5; h[13] = 5; h[14] = 5; h[15] = 4; h[16] = 2; h[17] = 1;
+		h[18] = 1; h[19] = 4; h[20] = 5; h[21] = 3; h[22] = 0; h[23] = 3; h[24] = 5; h[25] = 4; h[26] = 1;
+		h[27] = 2; h[28] = 5; h[29] = 3; h[30] = -12; h[31] = -24; h[32] = -12; h[33] = 3; h[34] = 5; h[35] = 2;
+		h[36] = 2; h[37] = 5; h[38] = 0; h[39] = -24; h[40] = -40; h[41] = -24; h[42] = 0; h[43] = 5; h[44] = 2;
+		h[45] = 2; h[46] = 5; h[47] = 3; h[48] = -12; h[49] = -24; h[50] = -12; h[51] = 3; h[52] = 5; h[53] = 2;
+		h[54] = 1; h[55] = 4; h[56] = 5; h[57] = 3; h[58] = 0; h[59] = 3; h[60] = 5; h[61] = 4; h[62] = 1;
+		h[63] = 1; h[64] = 2; h[65] = 4; h[66] = 5; h[67] = 5; h[68] = 5; h[69] = 4; h[70] = 2; h[71] = 1;
+		h[72] = 0; h[73] = 1; h[74] = 1; h[75] = 2; h[76] = 2; h[77] = 2; h[78] = 1; h[79] = 1; h[80] = 0;
+	}
 
- //    }
-	// float sum = 0;
+	double* v = new double[size * size];
+	float sigma = ceil(float(size)/6);
 	// for(int i=0;i<size;i++)
 	// {
 	// 	for(int j=0;j<size;j++)
 	// 	{
-	// 		sum += (float)h[i*size + j];
-	// 		cout << (float)h[i*size + j] << " ";
+	// 		int x = abs(i-mid);
+	// 		int y = abs(j-mid);
+	// 		float q = 2 * PI * pow(sigma, 2);
+	// 		int p = pow(x, 2) + pow(y, 2);
+	// 		h[i*size + j] = (float)(-(1/(PI * pow(sigma, 4))) * (1 - p/q) * exp(-p/q));
+	// 		h[i*size + j] *= 426.3;
+	// 		cout << -(1/(PI * pow(sigma, 4))) * (1 - p/q) * exp(-p/q) << " " << i << " " << j << " " << p << endl;
 	// 	}
-	// 	cout << endl;
 	// }
-	// cout << "Filter Sum" << " " << sum << endl;
-	// return convolve(image, h, size);	
+	int kernelSize = size;
+	for(int i = -(kernelSize/2); i<=(kernelSize/2); i++)
+    {
+
+        for(int j = -(kernelSize/2); j<=(kernelSize/2); j++)
+        {
+
+            double L_xy = -1/(PI * pow(sigma,4))*(1 - ((pow(i,2) + pow(j,2))/(2*pow(sigma,2))))*exp(-((pow(i,2) + pow(j,2))/(2*pow(sigma,2))));
+            //L_xy*=426.3;
+            v[(i + kernelSize/2)*size  + (j + kernelSize/2)] = L_xy;
+        }
+
+    }
+	float sum = 0;
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			sum += (float)v[i*size + j];
+			// cout << (float)v[i*size + j] << " ";
+		}
+		// cout << endl;
+	}
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			v[i*size + j] = v[i*size + j]/abs(sum);
+			// cout << (float)v[i*size + j] << " ";
+		}
+		// cout << endl;
+	}
+	cout << "Filter Sum" << " " << sum << endl;
+	return convolve(image, h, size, type);	
 }
 
 uint8_t* applyprewitt(Mat image, int size, int type)
@@ -528,12 +607,12 @@ int main()
 	createTrackbar("Kernel_size", "Tracker", u.kernel_size, 10, myFunc, &u);
 	Mat image = imread(imgs[id], IMREAD_GRAYSCALE);
 	Mat res_im( image.cols,image.rows, CV_8UC1, Scalar(255));
-	cv::putText(image, //target image
+	cv::putText(res_im, //target image
         "Move the track bars for output!", //text
         cv::Point(10, 600 / 2), //top-left position
         cv::FONT_HERSHEY_DUPLEX,
-        1.0,
-        CV_RGB(255, 255, 255), //font color
+        0.75,
+        CV_RGB(0, 0, 0), //font color
         2);
 	
 	Mat res(Size(image.cols*2,image.rows),CV_8UC1,Scalar::all(0));
